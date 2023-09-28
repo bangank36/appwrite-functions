7f69d93a7ca976898563ba8dcab015fd25a05182
bangank36 <bangank36@gmail.com>
2023-09-28 10:31:00 +0700

    Initial commit

---
+++ functions/starter-function/src/main.js
@@ -0,0 +1,37 @@
+// Ported: https://github.com/appwrite/templates/tree/main/node/starter
+
+const { Client } = require('node-appwrite');
+
+// This is your Appwrite function
+// It's executed each time we get a request
+module.exports = async function (context) {
+  const { req, res, log, error } = context;
+
+  // Why not try the Appwrite SDK?
+  //
+  // const client = new Client()
+  //   .setEndpoint('https://cloud.appwrite.io/v1')
+  //   .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
+  //   .setKey(process.env.APPWRITE_API_KEY);
+
+  // You can log messages to the console
+  log('Hello, Logs!');
+
+  // If something goes wrong, log an error
+  error('Hello, Errors!');
+
+  // The `req` object contains the request data
+  if (req.method === 'GET') {
+    // Send a response with the res object helpers
+    // `res.send()` dispatches a string back to the client
+    return res.send('Hello, World!');
+  }
+
+  // `res.json()` is a handy helper for sending JSON
+  return res.json({
+    motto: 'Build Fast. Scale Big. All in One Place.',
+    learn: 'https://appwrite.io/docs',
+    connect: 'https://appwrite.io/discord',
+    getInspired: 'https://builtwith.appwrite.io',
+  });
+};
